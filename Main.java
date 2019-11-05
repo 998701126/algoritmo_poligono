b41906705e75ff096f19905016d0bf2bb935e16c
Lopez Palomino Yuliana <palominoyeny@gmail.com>
2019-11-05 05:53:47 +0000

    Tarea_Finalizada

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args){
+
+	double a;
+  double b;
+	double c;
+  double d;
+  double e;
+  double f;
+  double g;
+  double h;
+  double i;
+  double j;
+
+	Scanner $ = new Scanner(System.in);
+
+	System.out.println("Por favor ingrese coordenada 01");
+	a= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 02");
+  b= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 03");
+  c= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 04");
+  d= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 05");
+  e= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 06");
+  f= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 07");
+  g= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 08");
+  h= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 09");
+  i= $.nextDouble();
+
+  System.out.println("Por favor ingrese coordenada 10");
+  j= $.nextDouble();
+
+  System.out.println("Su link es >> https://www.keene.edu/campus/maps/tool/?coordinates=-"+a+"%2C%20"+b+"%0A-"+c+"%2C%20"+d+"%0A-"+
+  e+"%2C%20"+f+"%0A-"+g+"%2C%20"+h+"%0A-"+i+"%2C%20"+j);
+
+	}
+
+}
